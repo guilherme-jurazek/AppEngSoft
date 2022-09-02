46ee0868bd5e4143860593177fe5be8c10afc4f4
taveira <taveira@WIN110>
2022-09-02 17:39:16 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppEngSoft.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
